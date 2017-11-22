2de0fc4302c9f7c25272a969d3fd3884e86d25bf
ruusty <russell@holliday.id.au>
2017-11-23 00:52:23 +1100

    Changed install path to use LOCALAPPDATA if HOMEDRIVE\HOMEPATH fails due to Invecto

--- tools/properties.ps1
+++ tools/properties.ps1
@@ -1,5 +1,19 @@
+# Set path that will not fail, 2 levels up from  LOCALAPPDATA
+#e.g. LOCALAPPDATA=C:\Users\Russell\AppData\Local
+# Should be a local disk
+$poshModFolder = ".PowershellModules"
+$installRootDirPath = $((Split-Path -Path $env:LOCALAPPDATA) | Split-Path) | Join-Path -child $poshModFolder
 
-$installRootDirPath    = $(join-path $(join-path $env:HOMEDRIVE $env:HOMEPATH) ".PowershellModules")
+# May fail because infecto disables network drive letters when in Elevated Privledge
+# Causes join-path to fail. Join-path does not fail when Drive is non-existant
+try
+{
+  $installRootDirPath = $(join-path -path $(join-path -path $env:HOMEDRIVE -child $env:HOMEPATH) -child $poshModFolder)
+}
+catch
+{
+  Write-Host $_
+}
 
 $moduleName="OraclePlSqlInstaller" #Top filepath in zip file
 
